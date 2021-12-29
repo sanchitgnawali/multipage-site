bffdbfee4a6dd57f7f390dd8f5760162d7b689d1
Sanchit Gnawali <sanchit.io@Sanchits-MBP.hsd1.tx.comcast.net>
2021-12-29 16:32:43 -0600

    Made article component to autoredirect to homepage on providing wrong id

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,12 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { useFetch } from "../hooks/useFetch";
 
 export default function Article() {
   const params = useParams();
   const { id } = params;
   const url = "http://localhost:3000/articles/" + id;
-
+  const navigate = useNavigate();
   const { data: article, isPending, error } = useFetch(url);
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => navigate("/"), 2000);
+    }
+  }, [error]);
 
   return (
     <div>
